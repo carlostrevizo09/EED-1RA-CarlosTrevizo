a3645b86c50d949b43d3a2f620d76f93831312e8
carlostrevizo09 <L25550019@chihuahua2.tecnm.mx>
2026-02-03 17:52:13 -0600

    Add files via upload

---
+++ EVA1_3_Stack_overflow/src/eva1_3_stack_overflow/EVA1_3_Stack_overflow.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_stack_overflow;
+
+/**
+ *
+ * @author carlo
+ */
+public class EVA1_3_Stack_overflow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+    }
+     public static void A(){
+         B();
+     }
+     public static void B(){
+         A();
+     }
+    
+}
---
+++ Eva1_2_Stack/src/eva1_2_stack/Eva1_2_Stack.java
@@ -0,0 +1,19 @@
+
+
+
+package eva1_2_stack;
+
+public class Eva1_2_Stack {
+
+    public static void main(String[] args) {
+       A();    
+    }
+    public static void A(){
+        B();
+    }
+    public static void B(){
+        int i = 5;
+        double b = 10.5;
+    }
+    
+}
